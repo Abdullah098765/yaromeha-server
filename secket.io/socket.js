27afc2d1e474a69b1d19ca9b35835f25602ccd7d
Abdullah <ssmmhazz@gmail.com>
2023-06-06 13:09:08 +0500

    'asd'

--- secket.io/socket.js
+++ secket.io/socket.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 export const socketIO = server => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: "https://yaromeha-server-production.up.railway.app",
       methods: ["GET", "POST"]
     }
   });
