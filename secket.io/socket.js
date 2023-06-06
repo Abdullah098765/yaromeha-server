074fbaf522075ed27971b961d941ff982ec1e7ae
Abdullah <ssmmhazz@gmail.com>
2023-06-06 13:58:07 +0500

    'userUid'

--- secket.io/socket.js
+++ secket.io/socket.js
@@ -16,6 +16,7 @@ export const socketIO = server => {
     if (change) {
       const groupId = change.documentKey._id.toString();
       io.emit(groupId, change);
+      console.log(change);
     }
   });
 
